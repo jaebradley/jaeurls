ca0cd942f771de3cb45923b8624cd90b1f23ffcd
jaebradley <jae.b.bradley@gmail.com>
2017-11-01 00:30:12 -0400

    update allowed origins

--- router/router.go
+++ router/router.go
@@ -20,7 +20,7 @@ func StartRouter(session *mgo.Session) {
 	http.Handle("/", r)
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost*"},
+		AllowedOrigins: []string{"http://localhost*", "https://jaebradley.github.io/jaeurls-presentation*"},
 	})
 
 	handler := c.Handler(r)
